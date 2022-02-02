8167df3ba9088411bcd4a8344b63f72d4a930878
Leonardo Borges <76793724+leonardob0rges@users.noreply.github.com>
2022-02-02 18:45:55 -0300

    Add files via upload

---
+++ password-gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import random
+import time
+    
+lower_letters = 'abcdefghijklmnopqrstuvwxyz'
+upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '1234567890'
+math_operators = '+-*/<>'
+symbols = '!@#%&*()`´{}[]^~:;.,\|/_'
+currencies='$£¢'
+
+all_chars = lower_letters + upper_letters + numbers + symbols + currencies
+length = int(input('[*] How password length you want: '))
+
+print('Generating password')
+for i in range(3):
+    print('.')
+    time.sleep(0.2)
+
+password = ''.join(random.sample(all_chars,length))
+print('\nYour password: ',password)
